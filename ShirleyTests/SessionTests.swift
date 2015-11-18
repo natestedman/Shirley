75206b253db65483e01902913af78c4486866152
Nate Stedman <natesm@gmail.com>
2015-11-17 22:56:11 -0500

    Add tests for Session.

---
+++ ShirleyTests/SessionTests.swift
@@ -0,0 +1,43 @@
+// Shirley
+// Written in 2015 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+import Shirley
+import XCTest
+
+class SessionTests: XCTestCase
+{
+    func testWrappedValue()
+    {
+        let wrapped = SquareSession()
+        let session = Session(session: wrapped)
+        
+        XCTAssertNotNil(wrapped.producerForRequest(2).first()?.value)
+        XCTAssertNotNil(session.producerForRequest(2).first()?.value)
+        
+        XCTAssertEqual(
+            wrapped.producerForRequest(2).first()?.value,
+            session.producerForRequest(2).first()?.value
+        )
+    }
+    
+    func testWrappedError()
+    {
+        let wrapped = ErrorSession()
+        let session = Session(session: wrapped)
+        
+        XCTAssertNotNil(wrapped.producerForRequest(2).first()?.error?.value)
+        XCTAssertNotNil(session.producerForRequest(2).first()?.error?.value)
+        
+        XCTAssertEqual(
+            wrapped.producerForRequest(2).first()?.error?.value,
+            session.producerForRequest(2).first()?.error?.value
+        )
+    }
+}
