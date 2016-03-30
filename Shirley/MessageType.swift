392b1e360d95fe477b09d4cb6ccb5a3b88391667
Nate Stedman <natesm@gmail.com>
2016-03-29 20:40:02 -0400

    Remove unnecessary “import Foundation”

--- Shirley/MessageType.swift
+++ Shirley/MessageType.swift
@@ -8,8 +8,6 @@
 // You should have received a copy of the CC0 Public Domain Dedication along with
 // this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-import Foundation
-
 /// A protocol type for response messages, implemented by `Message`.
 public protocol MessageType
 {
